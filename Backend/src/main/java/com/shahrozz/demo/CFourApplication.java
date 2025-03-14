72c1fdc327f55121db13c4157ae189ab7e6c5d17
ShahrozS <shahrozsalman7@gmail.com>
2025-03-14 18:01:01 +0500

    initializing backend

---
+++ Backend/src/main/java/com/shahrozz/demo/CFourApplication.java
@@ -0,0 +1,13 @@
+package com.shahrozz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CFourApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/shahrozz/demo/CFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahrozz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
